eba7e148ba7537c6bdf55b60ce81c0a4f380490c
ulziibat.n <ulziibat.n@gmail.com>
2025-11-30 13:30:06 +0800

    Add initial theme structure and template files
    - Created header.php for theme header structure.
    - Added template-functions.php for theme enhancements and custom functions.
    - Introduced template-tags.php for custom template tags.
    - Implemented index.php as the main template file.
    - Added readme.txt files for JavaScript and languages directories.
    - Created page.php for displaying all pages.
    - Added screenshot.png for theme preview.
    - Implemented search.php for search results pages.
    - Created single.php for displaying single posts.
    - Added content-excerpt.php for post archives and search results.
    - Implemented content-none.php for displaying a message when no posts are found.
    - Created content-page.php for displaying pages.
    - Added content-single.php for displaying single posts.
    - Implemented content.php for displaying posts.
    - Created footer-content.php for footer layout.
    - Added header-content.php for header layout.
    - Introduced theme.json for theme settings and color palette.

---
+++ theme/functions.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * ulziibat-tech functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package ulziibat-tech
+ */
+
+if ( ! defined( 'UB_VERSION' ) ) {
+	/*
+	 * Set the theme’s version number.
+	 *
+	 * This is used primarily for cache busting. If you use `npm run bundle`
+	 * to create your production build, the value below will be replaced in the
+	 * generated zip file with a timestamp, converted to base 36.
+	 */
+	define( 'UB_VERSION', '0.1.0' );
+}
+
+if ( ! defined( 'UB_TYPOGRAPHY_CLASSES' ) ) {
+	/*
+	 * Set Tailwind Typography classes for the front end, block editor and
+	 * classic editor using the constant below.
+	 *
+	 * For the front end, these classes are added by the `ub_content_class`
+	 * function. You will see that function used everywhere an `entry-content`
+	 * or `page-content` class has been added to a wrapper element.
+	 *
+	 * For the block editor, these classes are converted to a JavaScript array
+	 * and then used by the `./javascript/block-editor.js` file, which adds
+	 * them to the appropriate elements in the block editor (and adds them
+	 * again when they’re removed.)
+	 *
+	 * For the classic editor (and anything using TinyMCE, like Advanced Custom
+	 * Fields), these classes are added to TinyMCE’s body class when it
+	 * initializes.
+	 */
+	define(
+		'UB_TYPOGRAPHY_CLASSES',
+		'prose prose-neutral max-w-none prose-a:text-primary'
+	);
+}
+
+if ( ! function_exists( 'ub_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function ub_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on ulziibat-tech, use a find and replace
+		 * to change 'ulziibat-tech' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'ulziibat-tech', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in two locations.
+		register_nav_menus(
+			array(
+				'menu-1' => __( 'Primary', 'ulziibat-tech' ),
+				'menu-2' => __( 'Footer Menu', 'ulziibat-tech' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style-editor.css' );
+
+		// Add support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
+
+		// Remove support for block templates.
+		remove_theme_support( 'block-templates' );
+	}
+endif;
+add_action( 'after_setup_theme', 'ub_setup' );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function ub_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => __( 'Footer', 'ulziibat-tech' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your footer.', 'ulziibat-tech' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'ub_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function ub_scripts() {
+	wp_enqueue_style( 'ulziibat-tech-style', get_stylesheet_uri(), array(), UB_VERSION );
+	wp_enqueue_script( 'ulziibat-tech-script', get_template_directory_uri() . '/js/script.min.js', array(), UB_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'ub_scripts' );
+
+/**
+ * Enqueue the block editor script.
+ */
+function ub_enqueue_block_editor_script() {
+	$current_screen = function_exists( 'get_current_screen' ) ? get_current_screen() : null;
+
+	if (
+		$current_screen &&
+		$current_screen->is_block_editor() &&
+		'widgets' !== $current_screen->id
+	) {
+		wp_enqueue_script(
+			'ulziibat-tech-editor',
+			get_template_directory_uri() . '/js/block-editor.min.js',
+			array(
+				'wp-blocks',
+				'wp-edit-post',
+			),
+			UB_VERSION,
+			true
+		);
+		wp_add_inline_script( 'ulziibat-tech-editor', "tailwindTypographyClasses = '" . esc_attr( UB_TYPOGRAPHY_CLASSES ) . "'.split(' ');", 'before' );
+	}
+}
+add_action( 'enqueue_block_assets', 'ub_enqueue_block_editor_script' );
+
+/**
+ * Add the Tailwind Typography classes to TinyMCE.
+ *
+ * @param array $settings TinyMCE settings.
+ * @return array
+ */
+function ub_tinymce_add_class( $settings ) {
+	$settings['body_class'] = UB_TYPOGRAPHY_CLASSES;
+	return $settings;
+}
+add_filter( 'tiny_mce_before_init', 'ub_tinymce_add_class' );
+
+/**
+ * Limit the block editor to heading levels supported by Tailwind Typography.
+ *
+ * @param array  $args Array of arguments for registering a block type.
+ * @param string $block_type Block type name including namespace.
+ * @return array
+ */
+function ub_modify_heading_levels( $args, $block_type ) {
+	if ( 'core/heading' !== $block_type ) {
+		return $args;
+	}
+
+	// Remove <h1>, <h5> and <h6>.
+	$args['attributes']['levelOptions']['default'] = array( 2, 3, 4 );
+
+	return $args;
+}
+add_filter( 'register_block_type_args', 'ub_modify_heading_levels', 10, 2 );
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
---
+++ theme/template-parts/content/content-none.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template part for displaying a message when posts are not found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ulziibat-tech
+ */
+
+?>
+
+<section>
+
+	<header class="page-header">
+		<?php if ( is_search() ) : ?>
+
+			<h1 class="page-title">
+				<?php
+				printf(
+					/* translators: 1: search result title. 2: search term. */
+					'<h1 class="page-title">%1$s <span>%2$s</span></h1>',
+					esc_html__( 'Search results for:', 'ulziibat-tech' ),
+					get_search_query()
+				);
+				?>
+			</h1>
+
+		<?php else : ?>
+
+			<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'ulziibat-tech' ); ?></h1>
+
+		<?php endif; ?>
+	</header><!-- .page-header -->
+
+	<div <?php ub_content_class( 'page-content' ); ?>>
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+			?>
+
+			<p>
+				<?php esc_html_e( 'Your site is set to show the most recent posts on your homepage, but you haven&rsquo;t published any posts.', 'ulziibat-tech' ); ?>
+			</p>
+
+			<p>
+				<a href="<?php echo esc_url( admin_url( 'edit.php' ) ); ?>">
+					<?php
+					/* translators: 1: link to WP admin new post page. */
+					esc_html_e( 'Add or publish posts', 'ulziibat-tech' );
+					?>
+				</a>
+			</p>
+
+			<?php
+		elseif ( is_search() ) :
+			?>
+
+			<p>
+				<?php esc_html_e( 'Your search generated no results. Please try a different search.', 'ulziibat-tech' ); ?>
+			</p>
+
+			<?php
+			get_search_form();
+		else :
+			?>
+
+			<p>
+				<?php esc_html_e( 'No content matched your request.', 'ulziibat-tech' ); ?>
+			</p>
+
+			<?php
+			get_search_form();
+		endif;
+		?>
+	</div><!-- .page-content -->
+
+</section>
