d91f4d79eb177a71f39eb814cc47ba437a85ad97
ulziibat.n <ulziibat.n@gmail.com>
2025-11-30 14:48:19 +0800

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- theme/header.php
+++ theme/header.php
@@ -12,6 +12,7 @@
 
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,13 +20,13 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( 'font-sans' ); ?>>
 
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<div id="page">
-	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'ulziibat-tech' ); ?></a>
+	<div id="page">
+		<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'ulziibat-tech' ); ?></a>
 
-	<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
+		<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
 
-	<div id="content">
+		<div id="content">
